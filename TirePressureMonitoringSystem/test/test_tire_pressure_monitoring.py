4365314c504021d6ebf5db300fba0ad3f96fd350
SelrahcD <c.desneuf@gmail.com>
2024-05-28 13:29:34 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:TirePressureMonitoringSystem/test/test_tire_pressure_monitoring.py

--- TirePressureMonitoringSystem/test/test_tire_pressure_monitoring.py
+++ TirePressureMonitoringSystem/test/test_tire_pressure_monitoring.py
@@ -1,11 +1,18 @@
 import unittest
 
+from sensor import Sensor
 from tire_pressure_monitoring import Alarm
 
-class AlarmTest(Alarm):
+class TestableAlarm(Alarm):
 
-    def __init__(self):
+    def __init__(self, sensor):
         super().__init__()
+        self._sensor = sensor
+
+
+class SensorStub:
+    pass
+
 
 class AlarmTest(unittest.TestCase):
 
@@ -14,7 +21,9 @@ class AlarmTest(unittest.TestCase):
         assert not alarm.is_alarm_on
 
     def test__yyy(self):
-        alarm_test = AlarmTest()
+        sensor = SensorStub()
+        alarm_test = TestableAlarm(sensor)
+        alarm_test.check()
         assert not alarm_test.is_alarm_on
 
 
