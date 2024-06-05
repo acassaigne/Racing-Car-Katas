1acf0bfe182444cc034e7cf0ebf5b571dae77321
acassaigne <anthony.cassaigne@gmail.com>
2024-06-05 13:14:43 +0200

    mob next - wip
    lastFile:TextConverter/test/fixture_one_line.txt

--- TextConverter/test/test_text_converter.py
+++ TextConverter/test/test_text_converter.py
@@ -7,10 +7,15 @@ class UnicodeFileToHtmlTextConverterTest(unittest.TestCase):
         converter = UnicodeFileToHtmlTextConverter("foo")
         self.assertEqual("foo", converter.full_filename_with_path)
 
-    def test_convert_to_html(self):
+    def test_convert_to_html_with_an_empty_file(self):
         converter = UnicodeFileToHtmlTextConverter("fixture_1.txt")
         converted = converter.convert_to_html()
         self.assertEqual("", converted)
+
+    #TODO: test unexisting file
+    #TODO: test file with one line
+    #TODO: test file with two lines
+
 
 
 if __name__ == "__main__":
