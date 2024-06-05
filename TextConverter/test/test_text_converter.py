e7fe69c859a8858cff11ef7c35b180e9448e06f1
SelrahcD <c.desneuf@gmail.com>
2024-06-05 13:19:56 +0200

    test 2 green

--- TextConverter/test/test_text_converter.py
+++ TextConverter/test/test_text_converter.py
@@ -12,6 +12,11 @@ class UnicodeFileToHtmlTextConverterTest(unittest.TestCase):
         converted = converter.convert_to_html()
         self.assertEqual("", converted)
 
+    def test_convert_to_html_with_a_file_including_one_line(self):
+        converter = UnicodeFileToHtmlTextConverter("fixture_one_line.txt")
+        converted = converter.convert_to_html()
+        self.assertEqual("Hello<br />", converted)
+
     #TODO: test unexisting file
     #TODO: test file with one line
     #TODO: test file with two lines
