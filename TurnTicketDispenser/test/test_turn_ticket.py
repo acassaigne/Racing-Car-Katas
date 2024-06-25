bd47a5f4184ea77ae64ca505410d044f3a9742af
SelrahcD <c.desneuf@gmail.com>
2024-06-25 13:38:54 +0200

    WIP

--- TurnTicketDispenser/test/test_turn_ticket.py
+++ TurnTicketDispenser/test/test_turn_ticket.py
@@ -10,5 +10,16 @@ class TicketDispenserTest(unittest.TestCase):
         ticket = dispenser.getTurnTicket()
         self.assertEqual(TurnTicket(0), ticket)
 
+    @unittest.skip("Doesn't work because of static")
+    def test_do_somethingAA(self):
+        dispenser1 = TicketDispenser()
+        dispenser2 = TicketDispenser()
+        ticket0 = dispenser1.getTurnTicket()
+        ticket1 = dispenser1.getTurnTicket()
+        ticket2 = dispenser2.getTurnTicket()
+        self.assertEqual(TurnTicket(0), ticket0)
+        self.assertEqual(TurnTicket(1), ticket1)
+        self.assertEqual(TurnTicket(2), ticket2)
+
     def test_equal(self):
         self.assertEqual(TurnTicket(1), TurnTicket(1))
