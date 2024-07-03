9d9acf23304919b0c98326dc91486f4eec023406
SelrahcD <c.desneuf@gmail.com>
2024-07-03 13:33:07 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:TelemetrySystem/tests/test_telemetry.py

--- TelemetrySystem/tests/test_telemetry.py
+++ TelemetrySystem/tests/test_telemetry.py
@@ -2,9 +2,22 @@ import unittest
 
 from telemetry import *
 
+class StubAlwaysOfflineClient:
+    def __init__(self):
+        self.online_status = False
+
+    def disconnect(self):
+        pass
+
+    def connect(self, channel):
+        pass
+
+
 class TelemetryDiagnosticControlsTest(unittest.TestCase):
     def test_foo(self):
-        diagnostics = TelemetryDiagnostics()
-        # diagnostics.check_transmission()
-        self.assertEqual("", diagnostics.diagnostic_info)
+        diagnostics = TelemetryDiagnostics(StubAlwaysOfflineClient())
+        with self.assertRaisesRegex(Exception, "^Unable to connect\.$"):
+            diagnostics.check_transmission()
+
+        # self.assertEqual("", diagnostics.diagnostic_info)
         
