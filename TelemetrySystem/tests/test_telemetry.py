0f1c5ffdc7dd8faef0e239b3fb8f84b814e33bff
acassaigne <anthony.cassaigne@gmail.com>
2024-07-03 13:47:23 +0200

    mob next - wip
    lastFile:TelemetrySystem/tests/test_telemetry.py

--- TelemetrySystem/tests/test_telemetry.py
+++ TelemetrySystem/tests/test_telemetry.py
@@ -13,11 +13,33 @@ class StubAlwaysOfflineClient:
         pass
 
 
+class StubReceivingClient:
+    def __init__(self):
+        self.online_status = True
+        self.message_receive = ""
+
+    def disconnect(self):
+        pass
+
+    def connect(self, channel):
+        pass
+
+    def send(self, message):
+        self.message_receive = message
+
+    def receive(self):
+        return self.message_receive
+
 class TelemetryDiagnosticControlsTest(unittest.TestCase):
-    def test_foo(self):
+    def test_never_able_to_connect(self):
         diagnostics = TelemetryDiagnostics(StubAlwaysOfflineClient())
         with self.assertRaisesRegex(Exception, "^Unable to connect\.$"):
             diagnostics.check_transmission()
 
-        # self.assertEqual("", diagnostics.diagnostic_info)
-        
+    def test__bb(self):
+        stub = StubReceivingClient()
+        diagnostics = TelemetryDiagnostics(stub)
+        diagnostics.check_transmission()
+        self.assertEqual("AT#UD", diagnostics.diagnostic_info)
+
+#TODO: stub plus compliqué pour le retry
