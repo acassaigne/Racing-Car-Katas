0c4f26b4f265b4b8eff0a2ee37bd014a9d4a98e6
SelrahcD <c.desneuf@gmail.com>
2024-11-22 13:21:24 +0100

    WIP

--- Leaderboard/test/test_news.py
+++ Leaderboard/test/test_news.py
@@ -1,6 +1,6 @@
 import unittest
 
-from leaderboard import Leaderboard
+from leaderboard import Leaderboard, Driver, Race
 
 
 class TestCaracterisation(unittest.TestCase):
@@ -9,3 +9,11 @@ class TestCaracterisation(unittest.TestCase):
         l = Leaderboard(races=[])
 
         self.assertEquals({}, l.driver_points())
+
+    def test_when_no_races_no_driver_points_AAA(self):
+        driver1 = Driver(name="Nico Rosberg", country="DE")
+        race1 = Race("Australian Grand Prix", [driver1])
+
+        l = Leaderboard(races=[race1])
+
+        self.assertEqual({'Nico Rosberg': 25}, l.driver_points())
