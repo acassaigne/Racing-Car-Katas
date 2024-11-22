7c0eb6cbb8d73d71bebff3267225ae8461b9f51e
SelrahcD <c.desneuf@gmail.com>
2024-11-22 13:18:21 +0100

    WIP

--- Leaderboard/test/test_news.py
+++ Leaderboard/test/test_news.py
@@ -5,8 +5,7 @@ from leaderboard import Leaderboard
 
 class TestCaracterisation(unittest.TestCase):
 
-
     def test_when_no_races_no_driver_points(self):
         l = Leaderboard(races=[])
 
-        self.assertEquals("x", l.driver_points())
+        self.assertEquals({}, l.driver_points())
