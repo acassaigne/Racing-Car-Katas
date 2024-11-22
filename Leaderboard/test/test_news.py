c59e79ec4cb954094c2bba42624aa7659c741caf
acassaigne <anthony.cassaigne@gmail.com>
2024-11-22 14:02:58 +0100

    mob next - wip
    lastFile:test/test_news.py

--- Leaderboard/test/test_news.py
+++ Leaderboard/test/test_news.py
@@ -1,10 +1,20 @@
 import unittest
 
-from leaderboard import Leaderboard, Driver, Race
+import pytest
+
+from leaderboard import Leaderboard, Driver, Race, SelfDrivingCar
+
+
+@pytest.mark.parametrize("a,b", [("hello", "salut"),
+                                 ("foo", "bar")])
+def test_u(a, b):
+    assert a == b
 
 
 # Axes: more drivers / more races / more races and drivers / driver ranking / change driver type
 class TestCaracterisation(unittest.TestCase):
+
+
 
     def test_when_no_races_no_driver_points(self):
         l = Leaderboard(races=[])
@@ -19,10 +29,10 @@ class TestCaracterisation(unittest.TestCase):
 
         self.assertEqual({'Nico Rosberg': 25}, l.driver_points())
 
-    def test_when_winner_of_one_race_gets_25_points_with_a_self_driving_car(self):
-        driver1 = Driver(name="Nico Rosberg", country="DE")
+    def test_self_driving_car_name_is_display_whit_prefix_company_name_and_version(self):
+        driver1 = SelfDrivingCar(algorithm_version="1.2",company="acme")
         race1 = Race("Australian Grand Prix", [driver1])
 
         l = Leaderboard(races=[race1])
 
-        self.assertEqual({'Nico Rosberg': 25}, l.driver_points())
+        self.assertEqual({'Self Driving Car - acme (1.2)': 25}, l.driver_points())
